130072672b6c7f5e7e262aa02e32076470f7784c
Aaron Hoffman <kd0sgh@gmail.com>
2021-08-17 20:06:57 -0500

    Initial commit of FTEUlator

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="row q-pa-md justify-evenly">
+    <div class="col-6">
+      <q-card class="q-pa-md text-center">
+        <span class="text-h5">Welcome to the FTEUlator</span>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+});
+</script>
